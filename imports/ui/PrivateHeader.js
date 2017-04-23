4b573ad81f09c8b69e615323ea06e61397fd194a
Liubomyr Romankiv <liubomyrromankiv@gmail.com>
2017-04-23 13:58:43 +0300

    Initial commit

---
+++ imports/ui/PrivateHeader.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Accounts } from 'meteor/accounts-base';
+
+// export default class PrivateHeader extends React.Component {
+// 	onLogout() {
+//     Accounts.logout();
+//   }
+//   render() {
+//     return (
+//       <div>
+//       	<h1>{this.props.title}</h1>
+//         <button onClick={this.onLogout.bind(this)}>Logout</button>
+//       </div>
+//     );
+//   }
+// }
+
+const PrivateHeader = (props) => {
+	return (
+		<div className="header">
+			<div className="header__content">
+				<h1 className="header__title">{props.title}</h1>
+	      <button onClick={() => Accounts.logout() } className="button button--link-text">Logout</button>
+			</div>
+    </div>
+	);
+};
+
+PrivateHeaderpropTypes = {
+	title: React.PropTypes.string.isRequired
+}
+
+export default PrivateHeader;
